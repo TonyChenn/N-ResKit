6ff3918eb8d58bda4fc5055abd7b334fa2b339fd
TonyChenn <852454151@qq.com>
2022-12-27 14:21:30 +0800

    build assetBundle wnd

--- Editor/BuildWnd/BuildArguments.cs
+++ Editor/BuildWnd/BuildArguments.cs
@@ -2,105 +2,107 @@ using NCore.Editor;
 using UnityEditor;
 using UnityEngine;
 
-namespace AssetBundle
+//public enum BuildTargetEnum
+//{
+//    StandaloneWindows = BuildTarget.StandaloneWindows,
+//    StandaloneWindows64 = BuildTarget.StandaloneWindows64,
+//    StandaloneOSX = BuildTarget.StandaloneLinux64,
+//    //StandaloneLinux64 = BuildTarget.StandaloneLinux64,
+//    Android = BuildTarget.Android,
+//    iOS = BuildTarget.iOS,
+//}
+
+public class BuildArguments : IEditorPrefs
 {
-    public enum BuildTargetEnum
+
+    public BuildTarget CurBuildTarget
     {
-        StandaloneWindows = BuildTarget.StandaloneWindows,
-        StandaloneWindows64 = BuildTarget.StandaloneWindows64,
-        //StandaloneOSX = BuildTarget.StandaloneOSX,
-        //StandaloneLinux64 = BuildTarget.StandaloneLinux64,
-        Android = BuildTarget.Android,
-        iOS = BuildTarget.iOS,
+        get { return EditorUserBuildSettings.activeBuildTarget; }
+        set
+        {
+            if ((BuildTarget)value != EditorUserBuildSettings.activeBuildTarget)
+            {
+                EditorUtility.DisplayDialog("提示", "打包平台必须与激活平台保持一致", "好的");
+            }
+        }
+    }
+    /// <summary>
+    /// 是否转表
+    /// </summary>
+    public bool ConvertTable
+    {
+        get { return EditorPrefsHelper.GetBool("BuildArguments_ConvertTable", false, true); }
+        set { EditorPrefsHelper.SetBool("BuildArguments_ConvertTable", value, true); }
     }
 
-    public class BuildArguments : IEditorPrefs
+    /// <summary>
+    /// HybirdCLR
+    /// </summary>
+    public bool GenHybirdCLR
     {
-
-        public BuildTargetEnum CurBuildTarget
-        {
-            get { return EditorPrefsHelper.GetEnum<BuildTargetEnum>("BuildArguments_BuildTargetEnum", BuildTargetEnum.StandaloneWindows64, true); }
-            set { EditorPrefsHelper.SetEnum("BuildArguments_BuildTargetEnum", value, true); }
-        }
-        /// <summary>
-        /// 是否转表
-        /// </summary>
-        public bool ConvertTable
-        {
-            get { return EditorPrefsHelper.GetBool("BuildArguments_ConvertTable", false, true); }
-            set { EditorPrefsHelper.SetBool("BuildArguments_ConvertTable", value, true); }
-        }
-
-        /// <summary>
-        /// HybirdCLR
-        /// </summary>
-        public bool GenHybirdCLR
-        {
-            get { return EditorPrefsHelper.GetBool("BuildArguments_GenHybirdCLR", false,true); }
-            set { EditorPrefsHelper.SetBool("BuildArguments_GenHybirdCLR", value,true); }
-        }
-
-        /// <summary>
-        /// 打包工程
-        /// </summary>
-        public bool BuildProject
-        {
-            get { return EditorPrefsHelper.GetBool("BuildArguments_BuildProject", false,true); }
-            set { EditorPrefsHelper.SetBool("BuildArguments_BuildProject", value, true); }
-        }
-        /// <summary>
-        /// 全部重新打包（否则为增量打包）
-        /// </summary>
-        public bool BuildAll
-        {
-            get { return EditorPrefsHelper.GetBool("BuildArguments_BuildAll", false, true); }
-            set { EditorPrefsHelper.SetBool("BuildArguments_BuildAll", value, true); }
-        }
-
-        /// <summary>
-        /// 是否上传到服务器
-        /// </summary>
-        public bool UploadToFTP
-        {
-            get { return EditorPrefsHelper.GetBool("BuildArguments_UploadToFTP", false, true); }
-            set { EditorPrefsHelper.SetBool("BuildArguments_UploadToFTP", value, true); }
-        }
-
-        /// <summary>
-        /// 复制到StreammingAsset目录
-        /// </summary>
-        public bool CopyToSreeammingAsset
-        {
-            get { return EditorPrefsHelper.GetBool("BuildArguments_CopyToSreeammingAsset", false, true); }
-            set { EditorPrefsHelper.SetBool("BuildArguments_CopyToSreeammingAsset", value, true); }
-        }
-
-        /// <summary>
-        /// 打包UI
-        /// </summary>
-        public bool BuildUI
-        {
-            get { return EditorPrefsHelper.GetBool("BuildArguments_BuildUI", false, true); }
-            set { EditorPrefsHelper.SetBool("BuildArguments_BuildUI", value, true); }
-        }
-
-        /// <summary>
-        /// ExcelFolder
-        /// </summary>
-        public string ExcelFolder = "";
-
-        #region IEditorPrefs
-        public void ReleaseEditorPrefs()
-        {
-            EditorPrefsHelper.DeleteKey("BuildArguments_ConvertTable", true);
-            EditorPrefsHelper.DeleteKey("BuildArguments_GenHybirdCLR", true);
-            EditorPrefsHelper.DeleteKey("BuildArguments_BuildProject", true);
-            EditorPrefsHelper.DeleteKey("BuildArguments_BuildAll", true);
-            EditorPrefsHelper.DeleteKey("BuildArguments_UploadToFTP", true);
-            EditorPrefsHelper.DeleteKey("BuildArguments_CopyToSreeammingAsset", true);
-            EditorPrefsHelper.DeleteKey("BuildArguments_BuildUI", true);
-            EditorPrefsHelper.DeleteKey("BuildArguments_BuildTargetEnum", true);
-        }
-        #endregion
+        get { return EditorPrefsHelper.GetBool("BuildArguments_GenHybirdCLR", false, true); }
+        set { EditorPrefsHelper.SetBool("BuildArguments_GenHybirdCLR", value, true); }
     }
+
+    /// <summary>
+    /// 打包工程
+    /// </summary>
+    public bool BuildProject
+    {
+        get { return EditorPrefsHelper.GetBool("BuildArguments_BuildProject", false, true); }
+        set { EditorPrefsHelper.SetBool("BuildArguments_BuildProject", value, true); }
+    }
+    /// <summary>
+    /// 全部重新打包（否则为增量打包）
+    /// </summary>
+    public bool BuildAll
+    {
+        get { return EditorPrefsHelper.GetBool("BuildArguments_BuildAll", false, true); }
+        set { EditorPrefsHelper.SetBool("BuildArguments_BuildAll", value, true); }
+    }
+
+    /// <summary>
+    /// 是否上传到服务器
+    /// </summary>
+    public bool UploadToFTP
+    {
+        get { return EditorPrefsHelper.GetBool("BuildArguments_UploadToFTP", false, true); }
+        set { EditorPrefsHelper.SetBool("BuildArguments_UploadToFTP", value, true); }
+    }
+
+    /// <summary>
+    /// 复制到StreammingAsset目录
+    /// </summary>
+    public bool CopyToSreeammingAsset
+    {
+        get { return EditorPrefsHelper.GetBool("BuildArguments_CopyToSreeammingAsset", false, true); }
+        set { EditorPrefsHelper.SetBool("BuildArguments_CopyToSreeammingAsset", value, true); }
+    }
+
+    /// <summary>
+    /// 打包UI
+    /// </summary>
+    public bool BuildUI
+    {
+        get { return EditorPrefsHelper.GetBool("BuildArguments_BuildUI", false, true); }
+        set { EditorPrefsHelper.SetBool("BuildArguments_BuildUI", value, true); }
+    }
+
+    /// <summary>
+    /// ExcelFolder
+    /// </summary>
+    public string ExcelFolder = "";
+
+    #region IEditorPrefs
+    public void ReleaseEditorPrefs()
+    {
+        EditorPrefsHelper.DeleteKey("BuildArguments_ConvertTable", true);
+        EditorPrefsHelper.DeleteKey("BuildArguments_GenHybirdCLR", true);
+        EditorPrefsHelper.DeleteKey("BuildArguments_BuildProject", true);
+        EditorPrefsHelper.DeleteKey("BuildArguments_BuildAll", true);
+        EditorPrefsHelper.DeleteKey("BuildArguments_UploadToFTP", true);
+        EditorPrefsHelper.DeleteKey("BuildArguments_CopyToSreeammingAsset", true);
+        EditorPrefsHelper.DeleteKey("BuildArguments_BuildUI", true);
+    }
+    #endregion
 }
--- Editor/BuildWnd/BuildWindow.cs
+++ Editor/BuildWnd/BuildWindow.cs
@@ -5,187 +5,190 @@ using Table.Editor;
 using UnityEditor;
 using UnityEngine;
 
-namespace AssetBundle
+public class BuildWindow : EditorWindow
 {
-    public class BuildWindow : EditorWindow
+    private Vector2 pos;
+    private BuildArguments Args = new BuildArguments();
+    private GUIStyle titleStyle;
+    private string[] buildBundlePlaceArray = new string[] { "StreammingAsset", "AB包库", "自定义" };
+
+
+    #region life method
+    private void Awake()
     {
-        private Vector2 pos;
-        private BuildArguments Args = new BuildArguments();
-        private GUIStyle titleStyle;
-        private string[] buildBundlePlaceArray = new string[] { "StreammingAsset", "AB包库", "自定义" };
-
-
-        #region life method
-        private void Awake()
+        titleStyle = new GUIStyle();
+        titleStyle.normal.textColor = Color.red;
+        titleStyle.fontStyle = FontStyle.Bold;
+        titleStyle.fontSize = 20;
+    }
+    private void OnGUI()
+    {
+        pos = GUILayout.BeginScrollView(pos);
+        // 配置目录
+        GUILayout.Label("配置信息：", titleStyle);
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("AB包库：", GUILayout.Width(150));
+        Path_BuildBundle.SelectedBuildPlaceIndex = GUILayout.SelectionGrid(Path_BuildBundle.SelectedBuildPlaceIndex, buildBundlePlaceArray, buildBundlePlaceArray.Length);
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("", GUILayout.Width(150));
+        Path_BuildBundle.BuildBundleFolderPath = GUILayout.TextField(Path_BuildBundle.BuildBundleFolderPath);
+        if (Path_BuildBundle.SelectedBuildPlaceIndex == 0)
+            Path_BuildBundle.BuildBundleFolderPath = Application.streamingAssetsPath;
+        else if (Path_BuildBundle.SelectedBuildPlaceIndex == 1)
+            Path_BuildBundle.BuildBundleFolderPath = Path.GetFullPath($"{Application.dataPath}/../../AssetBundleDB/{Args.CurBuildTarget.ToString()}");
+        else if (Path_BuildBundle.SelectedBuildPlaceIndex == 2)
         {
-            titleStyle = new GUIStyle();
-            titleStyle.normal.textColor = Color.red;
-            titleStyle.fontStyle = FontStyle.Bold;
-            titleStyle.fontSize = 20;
+            if (GUILayout.Button("修改", GUILayout.Width(50)))
+            {
+                Path_BuildBundle.BuildBundleFolderPath = EditorUtility.SaveFolderPanel("选择导出AssetBundle目录", Path_BuildBundle.BuildBundleFolderPath, Path_BuildBundle.BuildBundleFolderPath);
+            }
         }
-        private void OnGUI()
+        GUILayout.EndHorizontal();
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("表格目录：", GUILayout.Width(150));
+        Args.ExcelFolder = GUILayout.TextField(Path_TableConfig.ExcelFolder);
+        if (GUILayout.Button("设置", GUILayout.Width(50)))
         {
-            pos = GUILayout.BeginScrollView(pos);
-            // 配置目录
-            GUILayout.Label("配置信息：", titleStyle);
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("AB包库：", GUILayout.Width(150));
-            BuildBundlePrefs.SelectedBuildPlaceIndex = GUILayout.SelectionGrid(BuildBundlePrefs.SelectedBuildPlaceIndex, buildBundlePlaceArray, buildBundlePlaceArray.Length);
-            GUILayout.EndHorizontal();
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("", GUILayout.Width(150));
-            BuildBundlePrefs.BuildBundleFolderPath = GUILayout.TextField(BuildBundlePrefs.BuildBundleFolderPath);
-            if (BuildBundlePrefs.SelectedBuildPlaceIndex == 0)
-                BuildBundlePrefs.BuildBundleFolderPath = Application.streamingAssetsPath;
-            else if (BuildBundlePrefs.SelectedBuildPlaceIndex == 1)
-                BuildBundlePrefs.BuildBundleFolderPath = Path.GetFullPath($"{Application.dataPath}/../../AssetBundleDB/{Args.CurBuildTarget.ToString()}");
-            else if (BuildBundlePrefs.SelectedBuildPlaceIndex == 2)
+            PackageWnd.ShowWnd(SettingPage.TAG, "导表配置");
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.Space(10);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("打包平台：", GUILayout.Width(150));
+        Args.CurBuildTarget = (BuildTarget)EditorGUILayout.EnumPopup(Args.CurBuildTarget);
+        GUILayout.EndHorizontal();
+        GUILayout.Space(30);
+
+
+
+        GUILayout.Label("打包参数：", titleStyle);
+        Args.BuildProject = GUILayout.Toggle(Args.BuildProject, "打包工程");
+        GUILayout.Space(5);
+
+        Args.BuildAll = GUILayout.Toggle(Args.BuildAll, "全部重新打包");
+        GUILayout.Space(5);
+
+        Args.ConvertTable = GUILayout.Toggle(Args.ConvertTable, "导入Excel表数据", GUILayout.Width(150));
+        GUILayout.Space(5);
+
+        Args.GenHybirdCLR = GUILayout.Toggle(Args.GenHybirdCLR, "生成HybirdCLR");
+        if(Args.BuildProject || Args.BuildAll)
+        {
+            Args.GenHybirdCLR = true;
+        }
+        GUILayout.Space(5);
+
+        Args.CopyToSreeammingAsset = GUILayout.Toggle(Args.CopyToSreeammingAsset, "复制Bundle到StreammingAsset");
+        GUILayout.Space(5);
+
+        Args.UploadToFTP = GUILayout.Toggle(Args.UploadToFTP, "上传资源");
+        GUILayout.Space(5);
+
+        Args.BuildUI = GUILayout.Toggle(Args.BuildUI, "打包UI");
+        GUILayout.Space(5);
+
+        GUILayout.EndScrollView();
+
+        if (GUILayout.Button("执行", GUILayout.Height(50)))
+        {
+            Build();
+        }
+    }
+    #endregion
+
+    #region editor menu
+    [MenuItem("Tools/AssetBundle/一键打包工具")]
+    public static void ShowWindow()
+    {
+        var window = Create();
+        window.titleContent = new GUIContent("一键打包");
+        window.minSize = new Vector2(500, 300);
+        window.Show();
+    }
+    public static BuildWindow Create()
+    {
+        return GetWindow<BuildWindow>();
+    }
+    #endregion
+
+    public void Build()
+    {
+        System.Collections.IEnumerator etor = BuildExcute(Args, true);
+        while (etor.MoveNext())
+        {
+            UnityEngine.Debug.Log(etor.Current);
+        }
+    }
+
+    public IEnumerator BuildExcute(BuildArguments args, bool showDialog)
+    {
+        if (showDialog)
+        {
+            if (!EditorUtility.DisplayDialog("一键打包工具", "确定要执行吗？", "确定", "取消"))
+                yield break;
+        }
+
+        //开始计时
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+
+        #region 打包流程
+        // TODO 转换配置表
+        if (args.ConvertTable)
+        {
+            yield return ">>>>>>>>>>执行转换配置表操作..";
+        }
+
+        if (Args.GenHybirdCLR)
+        {
+            BuildTarget target = Args.CurBuildTarget;
+            UnityEngine.Debug.Log("打包平台：" + target.ToString());
+            BuildDll.CompileDll(target);
+            BuildDll.BuildDllBundle(target);
+        }
+
+        // 创建打包的文件夹
+        if (!Directory.Exists(Path_BuildBundle.BuildBundleFolderPath))
+            Directory.CreateDirectory(Path_BuildBundle.BuildBundleFolderPath);
+
+        // 开始打包操作
+        if (Args.BuildProject)
+        {
+            var builder = BuildAB.Build(Path_BuildBundle.BuildBundleFolderPath, Args.BuildAll);
+            while (builder.MoveNext())
             {
-                if (GUILayout.Button("修改", GUILayout.Width(50)))
-                {
-                    BuildBundlePrefs.BuildBundleFolderPath = EditorUtility.SaveFolderPanel("选择导出AssetBundle目录", BuildBundlePrefs.BuildBundleFolderPath, BuildBundlePrefs.BuildBundleFolderPath);
-                }
+                yield return builder.Current;
             }
-            GUILayout.EndHorizontal();
-            GUILayout.Space(10);
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("表格目录：", GUILayout.Width(150));
-            Args.ExcelFolder = GUILayout.TextField(Path_TableConfig.ExcelFolder);
-            if (GUILayout.Button("设置", GUILayout.Width(50)))
-            {
-                PackageWnd.ShowWnd(SettingPage.TAG, "导表配置");
-            }
-            GUILayout.EndHorizontal();
-            GUILayout.Space(10);
-
-            GUILayout.BeginHorizontal();
-            GUILayout.Label("打包平台：", GUILayout.Width(150));
-            Args.CurBuildTarget = (BuildTargetEnum)EditorGUILayout.EnumPopup(Args.CurBuildTarget);
-            GUILayout.EndHorizontal();
-            GUILayout.Space(30);
+        }
+        AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
 
 
+        // 复制到StreammingAsset
+        if (Args.CopyToSreeammingAsset)
+        {
 
-            GUILayout.Label("打包参数：", titleStyle);
-            Args.BuildProject = GUILayout.Toggle(Args.BuildProject, "打包工程");
-            GUILayout.Space(5);
+            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
+        }
 
-            Args.BuildAll = GUILayout.Toggle(Args.BuildAll, "全部重新打包");
-            GUILayout.Space(5);
-
-            Args.ConvertTable = GUILayout.Toggle(Args.ConvertTable, "导入Excel表数据", GUILayout.Width(150));
-            GUILayout.Space(5);
-
-            Args.GenHybirdCLR = GUILayout.Toggle(Args.GenHybirdCLR, "生成HybirdCLR");
-            GUILayout.Space(5);
-
-            Args.CopyToSreeammingAsset = GUILayout.Toggle(Args.CopyToSreeammingAsset, "复制Bundle到StreammingAsset");
-            GUILayout.Space(5);
-
-            Args.UploadToFTP = GUILayout.Toggle(Args.UploadToFTP, "上传资源");
-            GUILayout.Space(5);
-
-            Args.BuildUI = GUILayout.Toggle(Args.BuildUI, "打包UI");
-            GUILayout.Space(5);
-
-            GUILayout.EndScrollView();
-
-            if (GUILayout.Button("执行", GUILayout.Height(50)))
-            {
-                Build();
-            }
+        // TODO 上传资源到服务器
+        if (args.UploadToFTP)
+        {
+            yield return ">>>>>>>>>>上传到服务器...";
         }
         #endregion
 
-        #region editor menu
-        [MenuItem("Tools/AssetBundle/一键打包工具")]
-        public static void ShowWindow()
+        watch.Stop();
+
+        int totalSeconds = (int)(watch.ElapsedMilliseconds / 1000f);
+        int minutes = Mathf.FloorToInt(totalSeconds / 60f);
+        int seconds = totalSeconds % 60;
+        if (showDialog)
         {
-            var window = Create();
-            window.titleContent = new GUIContent("一键打包");
-            window.minSize = new Vector2(500, 300);
-            window.Show();
-        }
-        public static BuildWindow Create()
-        {
-            return GetWindow<BuildWindow>();
-        }
-        #endregion
-
-        public void Build()
-        {
-            System.Collections.IEnumerator etor = BuildExcute(Args, true);
-            while (etor.MoveNext())
-            {
-                UnityEngine.Debug.Log(etor.Current);
-            }
-        }
-
-        public IEnumerator BuildExcute(BuildArguments args, bool showDialog)
-        {
-            if (showDialog)
-            {
-                if (!EditorUtility.DisplayDialog("一键打包工具", "确定要执行吗？", "确定", "取消"))
-                    yield break;
-            }
-
-            //开始计时
-            Stopwatch watch = new Stopwatch();
-            watch.Start();
-
-            #region 打包流程
-            // TODO 转换配置表
-            if (args.ConvertTable)
-            {
-                yield return ">>>>>>>>>>执行转换配置表操作..";
-            }
-
-            if (Args.GenHybirdCLR)
-            {
-                BuildTarget target = (BuildTarget)Args.CurBuildTarget;
-                UnityEngine.Debug.Log("打包平台：" + target.ToString());
-                BuildDll.CompileDll(target);
-                BuildDll.BuildDllBundle(target);
-            }
-
-            // 创建打包的文件夹
-            if (!Directory.Exists(BuildBundlePrefs.BuildBundleFolderPath))
-                Directory.CreateDirectory(BuildBundlePrefs.BuildBundleFolderPath);
-
-            // 开始打包操作
-            if (Args.BuildProject)
-            {
-                //var builder = BuildAB.Build(targetFolder, Args.BuildAll);
-                //while (builder.MoveNext())
-                //    yield return builder.Current;
-            }
-            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
-
-
-            // 复制到StreammingAsset
-            if (Args.CopyToSreeammingAsset)
-            {
-
-            }
-            AssetDatabase.Refresh(ImportAssetOptions.ForceUpdate);
-
-            // TODO 上传资源到服务器
-            if (args.UploadToFTP)
-            {
-                yield return ">>>>>>>>>>上传到服务器...";
-            }
-            #endregion
-
-            watch.Stop();
-
-            int totalSeconds = (int)(watch.ElapsedMilliseconds / 1000f);
-            int minutes = Mathf.FloorToInt(totalSeconds / 60f);
-            int seconds = totalSeconds % 60;
-            if (showDialog)
-            {
-                EditorUtility.DisplayDialog("打包完成", $"执行结束！耗时： {minutes} 分 {seconds} 秒。", "好的");
-            }
+            EditorUtility.DisplayDialog("打包完成", $"执行结束！耗时： {minutes} 分 {seconds} 秒。", "好的");
         }
     }
 }
